add09311ad4bb3563d83aad8aa22149adc3c1d83
liamm8777 <liamm8777@gmail.com>
2026-07-07 21:54:13 -0400

    Add program header and required imports

---
+++ Lab10_liam-1.py
@@ -0,0 +1,9 @@
+"""
+Program Name: Word Count Analyzer
+Author: Liam Malone
+Purpose: This program lets the user select one of four text files and counts how often each word appears.
+Date: July 8, 2026
+"""
+
+from pathlib import Path
+import string
