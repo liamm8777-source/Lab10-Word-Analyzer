3a169b125adce053e195dce1a31246f18b33574f
liamm8777 <liamm8777@gmail.com>
2026-07-07 22:05:51 -0400

    Add WordAnalyzer class structure

--- Lab10_liam-1.py
+++ Lab10_liam-1.py
@@ -2,8 +2,17 @@
 Program Name: Word Count Analyzer
 Author: Liam Malone
 Purpose: This program lets the user select one of four text files and counts how often each word appears.
+Starter Code: No starter code was used.
 Date: July 8, 2026
 """
 
 from pathlib import Path
 import string
+
+
+class WordAnalyzer:
+    """A class that analyzes a text file and counts word frequencies."""
+
+    def __init__(self, filepath):
+        self._filepath = Path(filepath)
+        self._frequencies = {}
